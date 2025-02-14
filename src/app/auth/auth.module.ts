f3ee2986644586621068ab7aef56d06bc45af14b
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2025-02-14 12:24:37 +0100

    Update auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 
-
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    FormsModule
+    FormsModule,
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class AuthModule { }
