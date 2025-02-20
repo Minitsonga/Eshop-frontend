1ed756a865845f7c77f29fae778165aa9d8eb459
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2025-02-20 17:35:04 +0100

    Fix routing + add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [Router]
 })
 export class AppComponent {
   title = 'e-shop-frontend';
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../auth/auth.service';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService, private router: Router) { }
+  authService = inject(AuthService);
+  router = inject(Router);
+  
+  cartItemCount = 2; // Exemple : à remplacer par une valeur dynamique venant du service panier
 
   logout() {
     this.authService.logout();
